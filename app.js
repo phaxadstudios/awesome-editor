1194a3394d2f355287848479735727fa940c7d54
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-03-22 13:49:20 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+/**
+ * 
+ * App.js is the javascript file
+ * it carries out the main functionality of this project
+ * Author: y2mate
+**/ 
+
+
+const first = document.querySelector('.first');
+const iframe = document.querySelector('.second');
+const button = document.querySelector('button');
+
+
+button.addEventListener("click", function(){
+	var html = first.textContent;
+	iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+})
+
+/** This function adds the no reload feature 
+ * which executes the codes while still writing 
+**/ 
+first.addEventListener('keyup', function(){
+	var html = first.textContent;
+	iframe.src = "data:text/html;charset=utf-8," + encodeURI(html);
+
+})
+/** end of function **/
+
+first.addEventListener('paste', function(e){
+	e.preventDefault();
+
+	var text = e.clipboardData.getData("text/plain");
+
+	document.execCommand("insertText", false,text);
+})
