f35c19d22fec5bf1e6e2601e61365302ef82a881
Nnamchi Israel <91748627+phaxadstudios@users.noreply.github.com>
2022-11-14 12:06:30 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,65 @@
  * it carries out the main functionality of this project
 **/ 
 
+/**
+ * 
+ * App.js is the javascript file
+ * it carries out the main functionality of this project
+**/ 
+
+
+
+  const setCaret = (pos, parent = el) => {
+    for (const node of parent.childNodes) {
+      if (node.nodeType == Node.TEXT_NODE) {
+        if (node.length >= pos) {
+          const range = document.createRange();
+          const sel = window.getSelection();
+          range.setStart(node, pos);
+          range.collapse(true);
+          sel.removeAllRanges();
+          sel.addRange(range);
+          return -1;
+        } else {
+          pos = pos - node.length;
+        }
+      } else {
+        pos = setCaret(pos, node);
+        if (pos < 0) {
+          return pos;
+        }
+      }
+    }
+    return pos;
+  };
+
+  highlight(el);
+
+  el.addEventListener('keydown', e => {
+    if (e.which === 9) {
+      const pos = caret() + tab.length;
+      const range = window.getSelection().getRangeAt(0);
+      range.deleteContents();
+      range.insertNode(document.createTextNode(tab));
+      highlight(el);
+      setCaret(pos);
+      e.preventDefault();
+    }
+  });
+
+  el.addEventListener('keyup', e => {
+    if (e.keyCode >= 0x30 || e.keyCode == 0x20) {
+      const pos = caret();
+      highlight(el);
+      setCaret(pos);
+    }
+  });
+};
+
+// Turn div into an editor
+const el = document.querySelector('.editor');
+el.focus();
+editor(el);
 
 
 
